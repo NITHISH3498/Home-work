360982e92b909241d56b23cd325e5d8f514b993d
NITHISH3498 <c.nithish3498@gmail.com>
2026-01-22 23:35:43 +0530

    java

---
+++ Day 23 Home Work/Anuj.java
@@ -0,0 +1,37 @@
+class Anuj
+{
+    public static void main(String[] args)
+    {
+        int arr[] = {10, 12, 14, 20, 50};
+        int left = 0;
+        int right = arr.length - 1;
+        int search = 14;
+
+        Anuj aj = new Anuj();
+        int index = aj.binary(arr, left, right, search);
+
+        System.out.println(index);
+    }
+
+    public int binary(int arr[], int left, int right, int search)
+    {
+        if (left <= right)
+        {
+            int mid = (left + right) / 2;
+
+            if (search == arr[mid])
+            {
+                return mid;
+            }
+            else if (search > arr[mid])
+            {
+                return binary(arr, mid + 1, right, search);
+            }
+            else
+            {
+                return binary(arr, left, mid - 1, search);
+            }
+        }
+        return -1;
+    }
+}
